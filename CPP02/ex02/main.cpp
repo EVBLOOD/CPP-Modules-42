1a211d2404c59f0bb28b71a83adc9a13f45c8c8b
Saad Akllam <sakllam@e3r10p5.1337.ma>
2022-06-24 12:07:39 +0100

    d02 end

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 14:12:30 by sakllam           #+#    #+#             */
-/*   Updated: 2022/06/24 11:54:18 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/06/24 12:07:20 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,21 @@ int main( void )
     std::cout << a << std::endl;
     std::cout << b << std::endl;
     std::cout << Fixed::max( a, b ) << std::endl;
+    // added tests
+    std::cout << "\\\\\\\\\\\\\\\\\n";
+    Fixed x(1);
+    Fixed y(2);
+    Fixed &c = Fixed::max(x, y);
+    std::cout << x * y << std::endl;
+    std::cout << x + y << std::endl;
+    std::cout << x - y << std::endl;
+    std::cout << x / y << std::endl;
+    std::cout << (x > y) << std::endl;
+    std::cout << (x >= y) << std::endl;
+    std::cout << (x < y) << std::endl;
+    std::cout << (x <= y) << std::endl;
+    std::cout << c << std::endl;
+    c = Fixed::min(x, y);
+    std::cout << c << std::endl;
     return 0;
 }
