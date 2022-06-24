bba052386aa2df82d7fb917ee9444749231b05e2
EVBLOOD <saad.akllam@gmail.com>
2022-06-24 16:02:06 +0100

    ex00

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 13:41:57 by sakllam           #+#    #+#             */
+/*   Updated: 2022/06/24 14:58:55 by sakllam          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+
+class ClapTrap
+{
+    private:
+        string name;
+        int     hit_points;
+        int     energy_points;
+        int     attack_damage;
+    public:
+        string getname(void) const;
+        ClapTrap(string name);
+        ~ClapTrap();
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
