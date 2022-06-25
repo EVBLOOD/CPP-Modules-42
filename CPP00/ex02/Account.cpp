06ae1e8abd8bd22f834f057f67d2f7e8cc8c0d69
EVBLOOD <saad.akllam@gmail.com>
2022-06-25 22:23:22 +0100

    now we work to push

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 16:46:05 by sakllam           #+#    #+#             */
-/*   Updated: 2022/06/22 15:46:26 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/06/25 22:22:36 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,13 @@ Account::Account( int initial_deposit )
     this->_accountIndex++;
     _totalNbDeposits++;
     _totalAmount += initial_deposit;
+    _displayTimestamp();
     std::cout << "index:" << this->_accountIndex << ";" << "Amount:" << this->_amount << ";created";
 }
 
 Account::~Account( void )
 {
+    this._displayTimestamp();
     std::cout << "index:" << this->_accountIndex << ";" << "Amount:" << this->_amount << ";closed";
 }
 
@@ -42,6 +44,56 @@ Account::Account(void)
     _totalNbDeposits = 0;
     _totalAmount = 0;
 }
+
+
+[19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
+[19920104_091532] index:1;amount:54;deposits:0;withdrawals:0
+[19920104_091532] index:2;amount:957;deposits:0;withdrawals:0
+[19920104_091532] index:3;amount:432;deposits:0;withdrawals:0
+[19920104_091532] index:4;amount:1234;deposits:0;withdrawals:0
+[19920104_091532] index:5;amount:0;deposits:0;withdrawals:0
+[19920104_091532] index:6;amount:754;deposits:0;withdrawals:0
+[19920104_091532] index:7;amount:16576;deposits:0;withdrawals:0
+
+////////////
+
+[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+[19920104_091532] index:1;p_amount:54;deposit:765;amount:819;nb_deposits:1
+[19920104_091532] index:2;p_amount:957;deposit:564;amount:1521;nb_deposits:1
+[19920104_091532] index:3;p_amount:432;deposit:2;amount:434;nb_deposits:1
+[19920104_091532] index:4;p_amount:1234;deposit:87;amount:1321;nb_deposits:1
+[19920104_091532] index:5;p_amount:0;deposit:23;amount:23;nb_deposits:1
+[19920104_091532] index:6;p_amount:754;deposit:9;amount:763;nb_deposits:1
+[19920104_091532] index:7;p_amount:16576;deposit:20;amount:16596;nb_deposits:1
+///////////////
+
+
+[19920104_091532] index:0;amount:47;deposits:1;withdrawals:0
+[19920104_091532] index:1;amount:819;deposits:1;withdrawals:0
+[19920104_091532] index:2;amount:1521;deposits:1;withdrawals:0
+[19920104_091532] index:3;amount:434;deposits:1;withdrawals:0
+[19920104_091532] index:4;amount:1321;deposits:1;withdrawals:0
+[19920104_091532] index:5;amount:23;deposits:1;withdrawals:0
+[19920104_091532] index:6;amount:763;deposits:1;withdrawals:0
+[19920104_091532] index:7;amount:16596;deposits:1;withdrawals:0
+[19920104_091532] index:0;p_amount:47;withdrawal:refused
+[19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
+[19920104_091532] index:2;p_amount:1521;withdrawal:657;amount:864;nb_withdrawals:1
+[19920104_091532] index:3;p_amount:434;withdrawal:4;amount:430;nb_withdrawals:1
+[19920104_091532] index:4;p_amount:1321;withdrawal:76;amount:1245;nb_withdrawals:1
+[19920104_091532] index:5;p_amount:23;withdrawal:refused
+[19920104_091532] index:6;p_amount:763;withdrawal:657;amount:106;nb_withdrawals:1
+[19920104_091532] index:7;p_amount:16596;withdrawal:7654;amount:8942;nb_withdrawals:1
+[19920104_091532] index:0;amount:47;deposits:1;withdrawals:0
+[19920104_091532] index:1;amount:785;deposits:1;withdrawals:1
+[19920104_091532] index:2;amount:864;deposits:1;withdrawals:1
+[19920104_091532] index:3;amount:430;deposits:1;withdrawals:1
+[19920104_091532] index:4;amount:1245;deposits:1;withdrawals:1
+[19920104_091532] index:5;amount:23;deposits:1;withdrawals:0
+[19920104_091532] index:6;amount:106;deposits:1;withdrawals:1
+[19920104_091532] index:7;amount:8942;deposits:1;withdrawals:1
+
+
 
 void	Account::makeDeposit( int deposit )
 {
@@ -100,7 +152,8 @@ int	Account::getNbWithdrawals( void )
     return (_totalNbWithdrawals);
 }
 
-// static void	displayAccountsInfos( void )
-// {
-//     // std::cout << this._accountIndex << ";" << "amount:" << this._amount << ";" <<
-// }
+static void	Account::displayAccountsInfos( void )
+{
+    _displayTimestamp();
+    std::cout << "accounts:" << this._nbAccounts << ";total:" << this->_totalAmount << ";deposits:" << this->_totalNbDeposits << ";withdrawals:" << this._totalNbWithdrawals << "\n";
+}
