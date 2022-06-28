6fd315b4bd127e703c1e9a16461952ef7ab84c31
Saad Akllam <sakllam@e3r10p4.1337.ma>
2022-06-28 22:45:12 +0100

    home

--- CPP04/ex01/Brain.hpp
+++ CPP04/ex01/Brain.hpp
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/28 21:33:37 by sakllam           #+#    #+#             */
-/*   Updated: 2022/06/28 22:43:10 by sakllam          ###   ########.fr       */
+/*   Created: 2022/06/28 22:38:00 by sakllam           #+#    #+#             */
+/*   Updated: 2022/06/28 22:40:57 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include "Brain.hpp"
 #include <string>
-#include <iostream>
+using std::string;
 
-class Animal
+class Brain
 {
-    protected:
-        std::string type;
-        Brain       dmagh;
+    private:
+        string ideas[100];
     public:
-        std::string getType(void) const;
-        Animal(std::string t);
-        Animal(Animal &a);
-        void setType(std::string type);
-        Animal *operator=(Animal &x);
-        Animal();
-        ~Animal();
-        virtual void makeSound(void) const;
+        Brain(/* args */);
+        ~Brain();
 };
+
+Brain::Brain(/* args */)
+{
+}
+
+Brain::~Brain()
+{
+}
