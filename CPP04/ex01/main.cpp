1f1c75039ea4ee0c90b753d9949cdb272f4aee3d
Saad Akllam <sakllam@e3r10p5.1337.ma>
2022-06-29 12:48:32 +0100

    ex01+

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 10:58:55 by sakllam           #+#    #+#             */
-/*   Updated: 2022/06/29 12:44:29 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/06/29 12:48:18 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,11 @@ int main()
     for (int i = 0; i < x; i++)
         delete okeh[i];
     delete[] okeh;
+    // ~not mine~
+    // const Animal* j = new Dog();
+    // const Animal* i = new Cat();
+    // delete j;//should not create a leak
+    // delete i;
     // system ("leaks prog");
+    return 0;
 }
